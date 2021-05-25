6f8936c9960d4420a155fd8b4a3f32f55045af0a
Adarsh Pal <adarshpal999@gmail.com>
2021-05-25 12:26:39 +0530

    Create 02_DecadeCounter.v

---
+++ 2. Circuits/2. Sequential Logic/2. Counters/02_DecadeCounter.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input reset,       
+    output [3:0] q
+  );
+  
+  always@(posedge clk) begin
+     if(reset | q==9)
+          q <= 0;
+      else
+          q <= q + 1'b1;
+  end
+
+endmodule
