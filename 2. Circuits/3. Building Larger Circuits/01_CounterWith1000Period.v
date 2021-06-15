50a97dd085fe2bdd7662f50b473dc5f884faf503
Adarsh Pal <adarshpal999@gmail.com>
2021-06-15 20:48:48 +0530

    Create 01_CounterWith1000Period.v

---
+++ 2. Circuits/3. Building Larger Circuits/01_CounterWith1000Period.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input reset,
+    output [9:0] q
+  );
+    
+    always @(posedge clk) begin
+        if (reset) q <= 0;
+        else begin
+          if (q < 999) q <= q + 1'b1;
+        	else q <= 0;
+        end
+    end
+
+endmodule
