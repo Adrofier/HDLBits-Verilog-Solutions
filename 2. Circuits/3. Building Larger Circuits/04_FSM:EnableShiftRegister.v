49f90fd79bd3f25bc704b877beadf46ef13c5bbe
Adarsh Pal <adarshpal999@gmail.com>
2021-06-16 12:18:34 +0530

    Update 04_FSM:EnableShiftRegister.v

--- 2. Circuits/3. Building Larger Circuits/04_FSM:EnableShiftRegister.v
+++ 2. Circuits/3. Building Larger Circuits/04_FSM:EnableShiftRegister.v
@@ -1,34 +1,26 @@
 module top_module (
     input clk,
     input reset,      // Synchronous reset
-    input data,
-    output start_shifting
-  );
-
-	localparam [2:0] IDLE = 0,
-					 S1   = 1,
-					 S11  = 2,
-					 S110 = 3,
-					 S1101= 4;
-
-	reg [2:0] state, next;
-
-	always @(*) begin
-		case (state)
-			IDLE  : next = (data) ? S1   : IDLE;
-			S1    : next = (data) ? S11  : IDLE;
-			S11   : next = (data) ? S11  : S110;
-			S110  : next = (data) ? S1101: IDLE;
-			S1101 : next = S1101;
-		endcase
-	end
-
-	always @(posedge clk) begin
-		if (reset) begin
-			state <= IDLE;
-		end
-		else state <= next;
-	end
-
-	assign start_shifting = (state == S1101);
+    output shift_ena
+	);
+    
+    parameter S0=3'd0, S1=3'd1, S2=3'd2, S3=3'd3, S4=3'd4;
+    reg [2:0]   state, next_state;
+    
+    always@(*) begin
+        case(state)
+            S0:     next_state = reset ? S1 : S0;
+            S1:     next_state = S2;
+            S2:     next_state = S3;
+            S3:     next_state = S4;
+            S4:     next_state = S0;
+        endcase
+    end
+    
+    always@(posedge clk) begin
+       state <= next_state; 
+    end
+    
+    assign shift_ena = (state==S1 | state==S2 | state==S3 | state==S4);
+    
 endmodule
